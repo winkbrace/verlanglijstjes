cabb41edfc2ef1083dabf5535fe51bad3848788c
Bas de Ruiter <deruiterb@gmail.com>
2022-09-09 13:16:47 +0200

    Add birthdate to users

--- database/factories/Verlanglijstjes/UserFactory.php
+++ database/factories/Verlanglijstjes/UserFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories\Verlanglijstjes;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-use JetBrains\PhpStorm\ArrayShape;
 use Verlanglijstjes\User;
 use function now;
 
@@ -15,18 +14,19 @@ class UserFactory extends Factory
     /**
      * Define the model's default state.
      */
-    #[ArrayShape(['name' => "string", 'email' => "string", 'email_verified_at' => "\Illuminate\Support\Carbon", 'password' => "string", 'remember_token' => "string", 'generation' => "int", 'position' => "int", 'chocolate_preference' => "string"])]
     public function definition(): array
     {
         return [
             'name' => $this->faker->unique()->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'birth_date' => $this->faker->date(),
             'generation' => 1,
             'position' => $this->faker->numberBetween(1, 4),
-            'chocolate_preference' => 'Melk'
+            'avatar' => null,
+            'chocolate_preference' => 'Melk',
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
         ];
     }
 
