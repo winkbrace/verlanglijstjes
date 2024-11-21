a6b10f5660183b441edc74f859e15c40980e4dbf
Bas de Ruiter <deruiterb@gmail.com>
2024-11-21 13:53:59 +0100

    Add New button to personal wish list

--- README.md
+++ README.md
@@ -34,6 +34,5 @@ Note: after running that script I manually updated avatars for people who got an
 # TODO
 
 1. Add errors layout and errors/403 and 404 pages
-2. Nieuw knop ook bovenaan bij lijstje naast naam
-3. Niet alle links werken (ici paris)
-4. https ssl certificate!
+3. Bij andermans lijstje wordt die knop "💡 Idee"
+4. Niet alle links werken (ici paris)
--- resources/views/components/wish-list.blade.php
+++ resources/views/components/wish-list.blade.php
@@ -1,8 +1,16 @@
 <div class="flex flex-col justify-around h-full">
     <div class="w-full max-w-2xl mx-auto bg-white shadow-lg rounded border border-yellow-300">
 
-        <header class="px-5 py-3 border-b border-yellow-300">
+        <header class="px-5 py-3 border-b border-yellow-300 flex justify-between">
             <h2 class="font-semibold text-gray-800">{{ $title }}</h2>
+            <x-nav-link :href="route('add-wish')">
+                <svg class="h-8 pb-1 mr-1 stroke-current" viewBox="0 0 24 24" fill="none">
+                    <path d="M12 8v3m0 0v3m0-3h3m-3 0H9" stroke-width="2" stroke-linecap="round"/>
+                    <path d="M14 19c3.771 0 5.657 0 6.828-1.172C22 16.657 22 14.771 22 11c0-3.771 0-5.657-1.172-6.828C19.657 3 17.771 3 14 3h-4C6.229 3 4.343 3 3.172 4.172 2 5.343 2 7.229 2 11c0 3.771 0 5.657 1.172 6.828.653.654 1.528.943 2.828 1.07" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M14 19c-1.236 0-2.598.5-3.841 1.145-1.998 1.037-2.997 1.556-3.489 1.225-.492-.33-.399-1.355-.212-3.404L6.5 17.5" stroke-width="2" stroke-linecap="round"/>
+                </svg>
+                {{ __('Nieuw') }}
+            </x-nav-link>
         </header>
 
         <ul class="flex flex-wrap justify-center divide-y divide-yellow-300">
