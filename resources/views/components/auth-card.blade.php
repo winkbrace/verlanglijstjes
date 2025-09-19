32b52a19481b368f1257f5d75114273cf28a821f
Bas de Ruiter <deruiterb@gmail.com>
2025-09-20 00:40:05 +0200

    Make login form compact so everything fits on mobile screen

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,7 +1,7 @@
 <div class="mt-4 md:mt-10 items-center sm:pt-0 bg-gray-100 md:w-80">
     <h1>{{ $title }}</h1>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden rounded-lg">
+    <div class="w-full sm:max-w-md mt-0 md:mt-6 px-6 py-4 bg-white shadow-md overflow-hidden rounded-lg">
         {{ $slot }}
     </div>
 </div>
